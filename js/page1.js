546602d45b631a94fd85819c28b2b8b5a9a88e70
radiostardom <126145799+radiostardom@users.noreply.github.com>
2024-05-07 22:15:39 -0700

    Initial commit

---
+++ js/page1.js
@@ -0,0 +1,81 @@
+var bg;
+var ufo;
+var ufolight;
+var arbys;
+var pole;
+var regular;
+var haunted;
+var font;
+var font2;
+
+let yOffset = 0;
+let amplitude = 20;
+let speed = 0.02;
+
+function setup() {
+  createCanvas(650, 2300);
+  font = loadFont('../font/Codystar-Regular.ttf');
+  font2 = loadFont('../font/CuteFont-Regular.ttf');
+  bg = loadImage('../img/lights.png');
+  ufo = loadImage('../img/ufo.png');
+  ufolight = loadImage('../img/ufolight.png');
+  arbys = loadImage('../img/glowingarbys.png');
+  pole = loadImage('../img/Pole.png');
+  regular = loadImage('../img/regular.png');
+  haunted = loadImage('../img/haunted.png');
+}
+
+function draw() {
+  background(bg);
+  
+  glow(color('#70D638B2'),100);
+  image(ufolight, -80, 90 + yOffset);
+  glow(color('#FDE089'), 0);
+  image(ufo, 0, -70 + yOffset);
+  image(pole, 320, 1000);
+  image(regular, 10, 2000); 
+  glow(color('#FFD556'), mouseY*0.1); 
+  image(arbys, 175, 800, 300, 300);  
+   
+  
+  yOffset = sin(frameCount * speed) * amplitude;
+  
+  fill('#FDE089');
+  textFont(font);
+  textSize(80);
+  textAlign(CENTER);
+  text('L\nI\nG\nH\nT\nS', 320, 300);
+
+  glow(color('#FDE089'), 0);
+  fill('#FFFFFF');
+  textFont(font2);
+  textSize(30);
+  textAlign(LEFT);
+  text('seen in the\nsky above\nthe Arby\'s.', 360, 1150+ (yOffset*0.5));
+  textAlign(RIGHT);
+  text('Not the\nglowing sign\nof Arby\'s;', 300, 1450+ (yOffset*0.5));
+  textAlign(LEFT);
+  text('something\nhigher and\nbeyond that.', 360, 1850+ (yOffset*0.5));
+  
+  if(mouseY > 2000){
+    glow(color('#4CAF50'), 100);
+    image(haunted, 10, 2000); 
+  }
+
+  if(mouseY < 2000){
+    cursor(ARROW);
+  } else {
+    cursor(HAND);
+  }
+}
+
+function mousePressed() {
+  if (mouseY > 2000) {
+    window.location.href = '../page2.html';
+  }
+}
+
+function glow(glowColor, blurriness){
+  drawingContext.shadowBlur = blurriness;
+  drawingContext.shadowColor = glowColor;
+}
---
+++ js/page2.js
@@ -0,0 +1,70 @@
+var phone;
+var bg;
+var hand;
+var font;
+var phoneX = -20; // Initial x-coordinate of the phone
+var phoneY = 50;   // Initial y-coordinate of the phone
+var shaking = false;
+var shakeTimer = 0;
+var shakeDuration = 30; // Adjust the duration of the shake
+var messages = ["...",
+  "WE KNOW THE DIFFERENCE.",
+  "WE'VE CAUGHT ONTO THEIR GAME.",
+  "WE UNDERSTAND THE LIGHTS-ABOVE-ARBY'S GAME."
+];
+var displayedMessages = [];
+
+function setup() {
+  createCanvas(650, 700);
+  phone = loadImage('../img/phone.png');
+  hand = loadImage('../img/hand.png');
+  font = loadFont('../font/CuteFont-Regular.ttf');
+  bg = loadImage('../img/bgtable.jpeg');
+}
+
+function draw() {
+  background(bg);
+  textFont(font);
+  
+  if(mouseY < 300){
+    cursor(ARROW);
+  } else {
+    cursor(HAND);
+  }
+  
+  // If shaking is true, apply a random displacement to the phone
+  if (shaking) {
+    var shakeAmount = 10; // Adjust the intensity of the shake
+    phoneX = -20 + random(-shakeAmount, shakeAmount);
+    phoneY = 50 + random(-shakeAmount, shakeAmount);
+    shakeTimer++;
+    if (shakeTimer >= shakeDuration) {
+      shaking = false;
+      shakeTimer = 0;
+    }
+  }
+  
+  // Display the phone image
+  image(phone, phoneX, phoneY);
+  
+  // Display the hand image
+  image(hand, mouseX-600, mouseY-100);
+  
+  // Display the messages at the top of the screen
+  textAlign(CENTER);
+  fill(255);
+  textSize(35);
+  for (var i = 0; i < displayedMessages.length; i++) {
+    text(displayedMessages[i], 312.5, 40 + i * 25);
+  }
+}
+
+function mouseClicked() {
+  // Check if the mouse click is within the phone's area
+  if (mouseX > phoneX && mouseX < phoneX + phone.width && mouseY > phoneY && mouseY < phoneY + phone.height) {
+    if (displayedMessages.length < messages.length) {
+      displayedMessages.push(messages[displayedMessages.length]);
+    }
+    shaking = true; // Start the shaking effect when the phone is clicked
+  }
+}
